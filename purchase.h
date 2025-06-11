e052b41a21943249b0f617273b9e16613d0aea22
SungjiKim <270643553@yoobeestudent.ac.nz>
2025-06-12 10:36:13 +1200

    change all code using for visual code in window

---
+++ purchase.h
@@ -0,0 +1,15 @@
+//
+// Created by MiaKim on 11/06/2025.
+//
+
+#ifndef PURCHASE_H
+#define PURCHASE_H
+
+#include "stockmanagement.h"
+
+#include <string>
+using namespace std;
+
+void purchaseProduct(const string& user_email);
+
+#endif //PURCHASE_H
---
+++ user_panel.cpp
@@ -0,0 +1,46 @@
+// Created by MiaKim on 11/06/2025.
+
+#include <iostream>
+#include "user_panel.h"
+#include "inventory.h"
+#include "purchase.h"
+#include "history.h"
+
+
+using namespace std;
+
+
+void userPanel(const string& user_email) {
+    // string user_email;
+    cout << "\nWelcome," << user_email << endl;
+
+    int choice;
+    while (true) {
+        cout << "\n======= User Panel =======" << endl;
+        cout << "1) View Inventory" << endl;
+        cout << "2) Purchase Product" << endl;
+        cout << "3) View Purchase History" << endl;
+        cout << "4) Logout" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            viewInventoryMenu(user_email);
+            break;
+        case 2:
+            purchaseProduct(user_email);
+            break;
+        case 3:
+            viewPurchaseHistory(user_email);
+            break;
+        case 4:
+            cout << "Logging out...\n";
+            return;
+        default:
+            cout << "Invalid choice. Please try again.\n";
+        }
+    }
+}
+
+
